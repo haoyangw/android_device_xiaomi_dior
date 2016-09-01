0e59890550179998d280d8bf1bb5afd1b3a93648
Ashish Kotnala <ashishkotnala1.ak@gmail.com>
2016-09-01 22:45:33 +0530

    use libqti-perfd-client.so instead of libqc-opt.so

--- device.mk
+++ device.mk
@@ -208,7 +208,7 @@ PRODUCT_PACKAGES += \
 # Qualcomm
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true \
-    ro.vendor.extension_library=/vendor/lib/libqc-opt.so
+    ro.vendor.extension_library=/vendor/lib/libqti-perfd-client.so
 
 # CMActions
 PRODUCT_PACKAGES += \
