29549b04d92bd28f382eb4c4d993f785042fb5df
Wang Hao Yang <haoyangw@gmail.com>
2016-11-15 18:04:13 +0800

    Remove CM-only references that does not exist in AOSP

--- power/power.c
+++ power/power.c
@@ -183,11 +183,6 @@ static void power_hint(__attribute__((unused)) struct power_module *module,
             }
         }
         break;
-    case POWER_HINT_SET_PROFILE:
-        pthread_mutex_lock(&lock);
-        set_power_profile(*(int32_t *)data);
-        pthread_mutex_unlock(&lock);
-        break;
     case POWER_HINT_LOW_POWER:
         /* This hint is handled by the framework */
         break;
@@ -199,15 +194,6 @@ static void power_hint(__attribute__((unused)) struct power_module *module,
 static struct hw_module_methods_t power_module_methods = {
     .open = NULL,
 };
-
-static int get_feature(__attribute__((unused)) struct power_module *module,
-                       feature_t feature)
-{
-    if (feature == POWER_FEATURE_SUPPORTED_PROFILES) {
-        return PROFILE_MAX;
-    }
-    return -1;
-}
 
 struct power_module HAL_MODULE_INFO_SYM = {
     .common = {
@@ -223,5 +209,4 @@ struct power_module HAL_MODULE_INFO_SYM = {
     .init = power_init,
     .setInteractive = power_set_interactive,
     .powerHint = power_hint,
-    .getFeature = get_feature
 };
